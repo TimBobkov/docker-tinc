50751c2495d5f5084f52f7761eda9bc9285ab59f
Тимофей Бобков <bobkov.tim@gmail.com>
2017-09-22 12:10:23 +0300

    bugs

--- tinc1.0/config/run.sh
+++ tinc1.0/config/run.sh
@@ -41,9 +41,9 @@ if [ $filecount -gt "0" ]; then
     done
 fi
 
-if [ ! $tinc/tinc-up ]; then cp /etc/default/tinc-up $tinc/tinc-up; fi
-if [ ! $tinc/tinc-down ]; then cp  /etc/default/tinc-down $tinc/tinc-down; fi
+if [ ! $tinc/tinc-up ]; then cp /etc/default/tinc-up $tinc/; fi
+if [ ! $tinc/tinc-down ]; then cp  /etc/default/tinc-down $tinc/; fi
 chmod +x $tinc/tinc-*
 
-tincd -D --logfile=/etc/tinc/tinc.log
+tincd -D --logfile=/etc/tinc/tinc.log -d 3
 
